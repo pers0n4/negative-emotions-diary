a09e90f6f79cd2a954477b376286929148fe20f6
Dong-Young Kim <31337.persona@gmail.com>
2022-04-13 01:33:01 +0900

    feat: mount static directories

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from .initializer import init_database, init_router
 
 app = FastAPI(
     title="Negative Emotions Diary API",
 )
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
 
 init_router(app)
 init_database(app)
