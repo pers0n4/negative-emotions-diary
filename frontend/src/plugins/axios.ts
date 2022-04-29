933b50715eba066964e635ba60f58d47a5f9bd00
Dong-Young Kim <31337.persona@gmail.com>
2022-04-29 21:20:53 +0900

    feat: create `axios` plugin

---
+++ frontend/src/plugins/axios.ts
@@ -0,0 +1,28 @@
+import Axios from "axios";
+
+const axios = Axios.create({
+  baseURL: "http://localhost:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axios.interceptors.request.use(
+  (config) => {
+    console.log("axios.js request : ", config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+axios.interceptors.response.use(
+  (res) => {
+    console.log("axios.js response : ", res);
+    return res;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+export default axios;
