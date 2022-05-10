aca724887a1634e302bd117fce2a6c0437b3fff1
Dong-Young Kim <31337.persona@gmail.com>
2022-05-11 00:19:09 +0900

    feat: allow cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .initializer import init_database, init_router
 
 app = FastAPI(
     title="Negative Emotions Diary API",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 init_router(app)
