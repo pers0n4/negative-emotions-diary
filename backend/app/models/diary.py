4f43018322f27fadeacabb835f8c62b86d6d4060
Dong-Young Kim <31337.persona@gmail.com>
2022-05-11 02:37:56 +0900

    feat: add negative affect from PANAS

--- backend/app/models/diary.py
+++ backend/app/models/diary.py
@@ -1,5 +1,7 @@
+
 from typing import TYPE_CHECKING
 
+from app.schemas.diary import Affect
 from tortoise import fields, models
 
 if TYPE_CHECKING:
@@ -10,6 +12,7 @@ class Diary(models.Model):
     id = fields.UUIDField(pk=True)
     title = fields.CharField(max_length=100)
     content = fields.TextField()
+    affect = fields.CharEnumField(Affect)
     user: fields.ForeignKeyRelation["User"] = fields.ForeignKeyField(
         "models.User", related_name="diaries"
     )
--- backend/app/schemas/diary.py
+++ backend/app/schemas/diary.py
@@ -1,23 +1,40 @@
+from enum import Enum
 from uuid import UUID
 
 from pydantic import BaseModel
 
 
+class Affect(str, Enum):
+    Hostile = "적대감"
+    Irritable = "짜증"
+    Ashamed = "부끄럼"
+    Guilty = "죄책감"
+    Distressed = "괴로움"
+    Upset = "화"
+    Scared = "겁"
+    Afraid = "두려움"
+    Jittery = "조바심"
+    Nervous = "불안"
+
+
 class DiaryCreate(BaseModel):
     title: str
     content: str
+    affect: Affect
 
 
 class DiaryRead(BaseModel):
     id: UUID
     title: str
     content: str
+    affect: Affect
 
 
 class Diary(BaseModel):
     id: UUID
     title: str
     content: str
+    affect: Affect
 
     user_id: UUID
 
