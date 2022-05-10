936d29941a16e374304fb1863d0f10a89a8e5962
Dong-Young Kim <31337.persona@gmail.com>
2022-05-11 00:20:38 +0900

    feat: use axios plugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -10,12 +10,16 @@ import "@quasar/extras/mdi-v6/mdi-v6.css";
 import "quasar/src/css/index.sass";
 
 import App from "./App.vue";
+import axios from "./plugins/axios";
 import router from "./router";
 
-createApp(App)
-  .use(router)
-  .use(Quasar, {
-    lang: quasarLang,
-    iconSet: quasarIconSet,
-  })
-  .mount("#app");
+const app = createApp(App);
+
+app.use(router).use(Quasar, {
+  lang: quasarLang,
+  iconSet: quasarIconSet,
+});
+
+app.config.globalProperties.$axios = axios;
+
+app.mount("#app");
