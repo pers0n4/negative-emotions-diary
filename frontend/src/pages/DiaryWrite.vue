1fd1b8ed4c46d5ab11c4fa2ec6566379ad828b71
Dong-Young Kim <31337.persona@gmail.com>
2022-06-08 05:31:42 +0900

    fix: update diray write page

--- frontend/src/pages/DiaryWrite.vue
+++ frontend/src/pages/DiaryWrite.vue
@@ -1,20 +1,18 @@
 <template>
   <q-page class="row justify-center items-center q-px-lg">
-    <div class="column">
+    <div class="column col-xs-12 col-sm-10 col-md-8">
       <q-chat-message
         text-html
         class="self-start"
         :text="[`<h5 class='no-margin'>오늘 하루는 무슨 일이 있었나요?</h5>`]"
       />
-      <q-chat-message text-html sent class="self-stretch" size="12">
-        <q-editor
-          v-model="editor"
-          :toolbar="[
-            ['bold', 'italic', 'strike', 'underline'],
-            ['undo', 'redo'],
-          ]"
-        />
-      </q-chat-message>
+      <q-editor
+        v-model="editor"
+        :toolbar="[
+          ['bold', 'italic', 'strike', 'underline'],
+          ['undo', 'redo'],
+        ]"
+      />
 
       <q-space class="q-my-sm" />
 
@@ -23,18 +21,7 @@
         text-html
         :text="[`<h5 class='no-margin'>지금 감정은 어떤가요?</h5>`]"
       />
-      <q-chat-message text-html sent class="self-end">
-        <div class="q-gutter-xs">
-          <q-chip
-            v-model:selected="chip.selected"
-            v-for="chip in chips"
-            :key="chip.key"
-            outline
-            square
-            >{{ chip.name }}</q-chip
-          >
-        </div>
-      </q-chat-message>
+      <q-select v-model="selectModel" :options="emotions" outlined />
 
       <q-btn color="primary" label="저장" class="q-mt-md" @click="save" />
     </div>
@@ -42,26 +29,27 @@
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 
 export default {
   setup() {
-    const chips = reactive([
-      { key: "Hostile", name: "적대감", selected: false },
-      { key: "Irritable", name: "짜증", selected: false },
-      { key: "Ashamed", name: "부끄럼", selected: false },
-      { key: "Guilty", name: "죄책감", selected: false },
-      { key: "Distressed", name: "괴로움", selected: false },
-      { key: "Upset", name: "화", selected: false },
-      { key: "Scared", name: "겁", selected: false },
-      { key: "Afraid", name: "두려움", selected: false },
-      { key: "Jittery", name: "조바심", selected: false },
-      { key: "Nervous", name: "불안", selected: false },
-    ]);
+    const emotions = [
+      { label: "적대감", value: "Hostile" },
+      { label: "짜증", value: "Irritable" },
+      { label: "부끄럼", value: "Ashamed" },
+      { label: "죄책감", value: "Guilty" },
+      { label: "괴로움", value: "Distressed" },
+      { label: "화", value: "Upset" },
+      { label: "겁", value: "Scared" },
+      { label: "두려움", value: "Afraid" },
+      { label: "조바심", value: "Jittery" },
+      { label: "불안", value: "Nervous" },
+    ];
 
     return {
       editor: ref(""),
-      chips,
+      selectModel: ref(null),
+      emotions,
     };
   },
   methods: {
