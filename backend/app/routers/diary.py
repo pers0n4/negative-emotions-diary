bc86674586b1579a682c8ba08eef73e8146dffd9
Dong-Young Kim <31337.persona@gmail.com>
2022-06-10 02:01:38 +0900

    feat: add analyze text logic

--- backend/app/routers/diary.py
+++ backend/app/routers/diary.py
@@ -2,6 +2,7 @@ from app.core.auth import get_current_user
 from app.models.diary import Diary
 from app.models.user import User
 from app.schemas.diary import DiaryCreate, DiaryRead
+from app.services.comprehend import comprehend
 from fastapi import APIRouter, Depends, Query, status
 
 router = APIRouter(
@@ -30,7 +31,19 @@ async def create_diary(
     diary_body: DiaryCreate,
     current_user: User = Depends(get_current_user),
 ):
-    diary = await Diary.create(
-        **diary_body.dict(exclude_unset=True), user_id=current_user.id
+    sentiments = comprehend.detect_sentiment(Text=diary_body.content, LanguageCode="ko")
+    key_phrases = comprehend.detect_key_phrases(
+        Text=diary_body.content, LanguageCode="ko"
     )
+    entities = comprehend.detect_entities(Text=diary_body.content, LanguageCode="ko")
+
+    diary = await Diary.create(
+        **diary_body.dict(exclude_unset=True),
+        user_id=current_user.id,
+        sentiment=sentiments["Sentiment"],
+        sentiment_score=sentiments["SentimentScore"],
+        entities=entities["Entities"],
+        key_phrases=key_phrases["KeyPhrases"],
+    )
+
     return diary
